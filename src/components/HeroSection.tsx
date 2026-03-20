307a64e327996632b69988afca32cfcde3ce4ea9
Eduardo Passos <edupassosa18@gmail.com>
2026-03-19 22:52:59 -0300

    Pequena mudança em Landing, e página de Login iniciada.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
         <div className="w-full h-[calc(50dvh-(44.1px+36px))] bg-[#22284b]">
             <div className="grid grid-cols-3 h-full w-full">
                 <div className="col-span-2 content-center">
-                    <div className="w-fit ml-[2dvw] mr-auto justify-items-center">
+                    <div className="w-fit ml-[2dvw] mr-auto justify-items-center space-y-2">
                         <h1 className="text-white font-bold text-6xl">Site bom</h1>
                         <h2 className="text-neutral-400 font-semibold">Isso é um exemplo de site de "alta" qualidade.</h2>
                         <Button 
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,7 +1,11 @@
-import Nav from '../Nav.tsx';
+
 
 export default function Login() {
     return(
-        <Nav />
+        <div className="size-dvh">
+            <div className='flex flex-col size-2/5 min-h-72 min-w-72 border border-white bg-[#22284b] drop-shadow-xl/50 rounded-2xl'>
+
+            </div>
+        </div>
     )
 }
